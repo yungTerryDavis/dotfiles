9ccdbdecdc4cbc6784475223f9e571ae61f9d566
yungTerryDavis <chulkov.nikita01@gmail.com>
2026-09-05 16:16:35 +0700

    feat(nvim): set up lua and python lsp

---
+++ nvim/.config/nvim/lua/ngumbk/plugins/lsp.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
+    config = function()
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("basedpyright")
+      vim.diagnostic.config({
+        virtual_text = true,
+      })
+    end,
+  }
+}
